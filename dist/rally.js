a9f90334410b67d23a4de826f8360bce0f4920df
pcw216 <pwinters@rallydev.com>
2014-07-12 14:12:53 -0400

    Rebuild and some grunt/config changes due to folder moves

--- dist/rally.js
+++ dist/rally.js
@@ -1,3 +1,109 @@
+angular.module('rally.api', ['rally.api.services']);
+
+angular.module('rally.api.services', ['rally.api.services.slm', 'rally.api.services.wsapi', 'rally.api.services.lookback']);
+
+/**
+ * @ngdoc service
+ * @name rally.api.services:$lookback
+ * @description
+ * @example
+*/
+
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+angular.module('rally.api.services.lookback', []).provider('$lookback', function() {
+  var LookbackApi,
+    _this = this;
+  this.baseUrl = '/analytics/v2.0/service/rally';
+  this.setBaseUrl = function(baseUrl) {
+    _this.baseUrl = baseUrl;
+  };
+  LookbackApi = (function() {
+    function LookbackApi($http, baseUrl) {
+      this.$http = $http;
+      this.baseUrl = baseUrl;
+      this.artifactSnapshots = __bind(this.artifactSnapshots, this);
+    }
+
+    LookbackApi.prototype.artifactSnapshots = function(workspaceOid, query, config) {
+      return this.$http.post("" + this.baseUrl + "/workspace/" + workspaceOid + "/artifact/snapshot/query", JSON.stringify(query), config);
+    };
+
+    return LookbackApi;
+
+  })();
+  this.$get = function($http) {
+    return new LookbackApi($http, _this.baseUrl);
+  };
+  return this;
+});
+
+/**
+ * @ngdoc service
+ * @name rally.api.services:$slm
+ * @description
+ * @example
+*/
+
+angular.module('rally.api.services.slm', ['rally.util.http']).provider('$slm', function() {
+  var _this = this;
+  this.baseUrl = '/slm/';
+  this.setBaseUrl = function(baseUrl) {
+    _this.baseUrl = baseUrl;
+  };
+  this.$get = function(rallyHttpWrapper) {
+    return rallyHttpWrapper(_this.baseUrl);
+  };
+  return this;
+});
+
+/**
+ * @ngdoc service
+ * @name rally.api.services:$wsapi
+ * @description
+ * Abstracts $http calls to a wsapi base url. Can be configured at config/provider time to use a particular base url.
+ * @example
+		<example module="App">
+				<file name="script.js">
+						angular.module('App', ['rally.api.services.wsapi'])
+						.config(function($wsapiProvider){
+							$wsapiProvider.setBaseUrl('/test/');
+						})
+						.controller('Ctrl',
+							function Ctrl($scope, $httpBackend, $wsapi) {
+								$scope.$wsapi = $wsapi;
+								console.log('backend', $httpBackend)
+								console.log('backend.whenGET', $httpBackend.whenGET)
+								$httpBackend.whenGET('/test/sub/url').respond(200, {foo: 'bar'});
+								$wsapi({method: 'GET', url: '/sub/url/'}).then(function(data, status){
+									$scope.data = data
+									$scope.status = status
+								});
+							}
+						);
+				</file>
+				<file name="index.html">
+						<div ng-controller="Ctrl">
+							<div>Wsapi base url: {{$wsapi.baseUrl}}</div>
+							<div>data: {{data}}</div>
+							<div>status: {{status}}</div>
+						</div>
+				</file>
+		</example>
+*/
+
+angular.module('rally.api.services.wsapi', ['rally.api.services.slm', 'rally.util.http']).provider('$wsapi', function() {
+  var _this = this;
+  this.baseUrl = '/webservice/v2.0/';
+  this.setBaseUrl = function(baseUrl) {
+    _this.baseUrl = baseUrl;
+  };
+  this.$get = function(rallyHttpWrapper, $slm) {
+    return rallyHttpWrapper(_this.baseUrl, $slm);
+  };
+  return this;
+});
+
 var __slice = [].slice;
 
 angular.module('rally.app.iframe.decorators.rootScope', []).config(function($provide) {
@@ -79,28 +185,16 @@ angular.module('rally.app.iframe.services.messageBus', ['rally.app.iframe.decora
 
 angular.module('rally.app.iframe.services', ['rally.app.iframe.services.messageBus', 'rally.app.iframe.services.appService']);
 
-angular.module('rally.app', []);
+angular.module('rally.app', ['rally.app.services']);
 
+angular.module('rally.app.services', ['rally.app.services.rally']);
 
-
-angular.module('Ext', []).factory('Ext', function() {
-  return Ext;
-});
-
-angular.module('Rally', []).factory('Rally', function() {
-  return Rally;
-});
-
-angular.module('rally', ['rally.services', 'rally.app']);
-
-angular.module('rally.services', ['rally.services.rally']);
-
-var RallyService,
+var RallyAppService,
   __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
   __slice = [].slice;
 
-angular.module('rally.services.rally', ['Ext', 'Rally']).service('$rally', RallyService = (function() {
-  function RallyService($q, $rootScope, $log, Ext, Rally) {
+angular.module('rally.app.services.rally', ['Ext', 'Rally']).service('$rally', RallyAppService = (function() {
+  function RallyAppService($q, $rootScope, $log, Ext, Rally) {
     this.$q = $q;
     this.$rootScope = $rootScope;
     this.$log = $log;
@@ -110,7 +204,7 @@ angular.module('rally.services.rally', ['Ext', 'Rally']).service('$rally', Rally
     this.launchApp = __bind(this.launchApp, this);
   }
 
-  RallyService.prototype.launchApp = function(appName, options, scope) {
+  RallyAppService.prototype.launchApp = function(appName, options, scope) {
     var defaults, deferred, self, theApp;
     if (options == null) {
       options = {};
@@ -137,7 +231,7 @@ angular.module('rally.services.rally', ['Ext', 'Rally']).service('$rally', Rally
     return deferred.promise;
   };
 
-  RallyService.prototype.bind = function(scope, observable) {
+  RallyAppService.prototype.bind = function(scope, observable) {
     var _this = this;
     this.$log.debug('Binding to Rally app events');
     Ext.util.Observable.capture(observable, function() {
@@ -152,6 +246,148 @@ angular.module('rally.services.rally', ['Ext', 'Rally']).service('$rally', Rally
     return observable;
   };
 
-  return RallyService;
+  return RallyAppService;
 
 })());
+
+
+
+angular.module('Ext', []).factory('Ext', function() {
+  return Ext;
+});
+
+angular.module('Rally', []).factory('Rally', function() {
+  return Rally;
+});
+
+angular.module('rally', ['rally.api', 'rally.app', 'rally.util']);
+
+angular.module('rally.util.async', []).factory('async', function() {
+  return async;
+});
+
+/**
+ * @ngdoc service
+ * @name rally.util.cache:$cacheWrap
+ * @type function
+ * @description
+ * Helper function that returns a cached value or runs your function to cache the result and return your value as a promise.
+ * If you value function returns a promise, the wrapper will wait for resolution and put the result in your cache.
+*/
+
+angular.module('rally.util.cache.factories.wrap', ['rally.util.lodash']).factory('$cacheWrap', function($q) {
+  var $cacheWrap;
+  $cacheWrap = function(cache, keyFn) {
+    if (keyFn == null) {
+      keyFn = _.identity;
+    }
+    return function(key, func) {
+      var deferred;
+      key = keyFn(key);
+      if (cache.get(key)) {
+        return $q.when(cache.get(key));
+      }
+      deferred = $q.defer();
+      cache.put(key, deferred.promise);
+      deferred.promise.then(function(result) {
+        return cache.put(key, result);
+      }, function() {
+        return cache.remove(key);
+      });
+      deferred.resolve($q.when(func()));
+      return deferred.promise;
+    };
+  };
+  return $cacheWrap;
+});
+
+angular.module('rally.util.cache', ['rally.util.cache.factories.wrap']);
+
+var __slice = [].slice;
+
+angular.module('rally.util.http.factories.httpWrapper', ['rally.util.lodash']).factory('rallyHttpWrapper', function($http) {
+  return function(baseUrl, http) {
+    var getUrl, wrapper;
+    if (http == null) {
+      http = $http;
+    }
+    getUrl = function(url) {
+      var toAdd;
+      toAdd = url;
+      if (baseUrl[baseUrl.length - 1] === '/' && url.indexOf('/') === 0) {
+        toAdd = toAdd.substring(1);
+      }
+      return baseUrl + toAdd;
+    };
+    wrapper = function(urlOrConfig, config) {
+      if (_.isString(urlOrConfig)) {
+        urlOrConfig = getUrl(urlOrConfig);
+      } else {
+        urlOrConfig.url = getUrl(urlOrConfig.url);
+      }
+      return http.call(this, urlOrConfig, config);
+    };
+    _.each(['GET', 'DELETE', 'POST', 'PUT'], function(method) {
+      return wrapper[method.toLowerCase()] = function() {
+        var args, url, _ref;
+        url = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+        url = getUrl(url);
+        return (_ref = http[method.toLowerCase()]).call.apply(_ref, [this, url].concat(__slice.call(args)));
+      };
+    });
+    return wrapper;
+  };
+});
+
+angular.module('rally.util.http', ['rally.util.http.factories.httpWrapper']);
+
+angular.module('rally.util.lodash', ['rally.util.lodash.sortedInsert']);
+
+angular.module('rally.util.lodash.sortedInsert', []).run(function() {
+  _.sortedInsert = function(array, value, pluck) {
+    var index;
+    index = _.sortedIndex(array, value, pluck);
+    array.splice(index, 0, value);
+    return array;
+  };
+  return _.sortedReverseInsert = function(array, value, pluck) {
+    _.sortedInsert(array.reverse(), value, pluck);
+    return array.reverse();
+  };
+});
+
+var __slice = [].slice;
+
+angular.module('rally.util.timeout', []).factory('$rallyTimeoutThrottleFactory', function($timeout) {
+  return function(max) {
+    var processNext, queue, running;
+    queue = [];
+    running = 0;
+    processNext = function() {
+      var args, context, next;
+      if (running >= max) {
+        return;
+      }
+      next = queue.shift();
+      context = next[0];
+      args = next.slice(1);
+      running = running + 1;
+      return $timeout.apply(context, args)['finally'](function() {
+        running = running - 1;
+        return processNext();
+      });
+    };
+    return function() {
+      var args;
+      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
+      queue.push([this].concat(args));
+      return processNext();
+    };
+  };
+}).factory('$rallyTimeoutThrottle', function($rallyTimeoutThrottleFactory) {
+  return $rallyTimeoutThrottleFactory(10);
+});
+
+var util;
+
+util = angular.module('rally.util', ['rally.util.async', 'rally.util.cache', 'rally.util.http', 'rally.util.lodash', 'rally.util.timeout']);
